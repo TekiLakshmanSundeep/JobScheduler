fb88bc532cd46a48653b0618ef1573b4db80a284
Durga Bhavani M <durgabhavani9699@gmail.com>
2021-07-12 19:22:48 +0530

    Implemented send grid mails

--- worker/db_worker.php
+++ worker/db_worker.php
@@ -16,7 +16,37 @@ require_once "./send_email.php";
          $comic_url = $data."/info.0.json";
          $json = file_get_contents($comic_url);
          $json_data = json_decode($json, true);
-         send_inline_image($json_data, $comic_url);
+
+         $message =
+         '<html>
+             <head>
+                 <style> p {color:green};
+                 img {
+         
+                 }
+                     .heading {
+                         text-align: left;
+                         color: #ff9800;
+                     }
+                     .day_count {
+                         text-align: left;
+                     }
+                 </style>
+             </head>
+             <body>
+                 
+                 <h3 class="heading">'.$json_data['title'].'</h3>
+                 <p class="day_count"><b><i> Day '.$json_data['day'].':'.$json_data['alt'].'</i></b></p>
+                 <br>
+                     <img src="'.$json_data['img'].'">
+                 <br>
+                 <p>To unsubscribe to the comics <a href="localhost/projects/rtcamp/unsub.php?key=durgabhavani9360@gmail.com">Click here</a></p>
+                 <br>
+                     
+             </body>
+         </html>'."\n\n";
+         $attachment_details = array($json_data['img'], $json_data['title']);
+         SendEmail::SendMail("funnysunny.teki@gmail.com","rtCamp Comics", $attachment_details);
          return false;
      }
  }
