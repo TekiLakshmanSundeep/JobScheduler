3a7c7a856f4d1e5fcf6cd985b14c00001f3443e8
Durga Bhavani M <durgabhavani9699@gmail.com>
2021-07-14 21:03:45 +0530

    Sample mail file

--- send_email.php
+++ send_email.php
@@ -5,30 +5,30 @@
      
      class SendEmail{
      public static function SendMail($to,$subject,$content, $attachment_content){
-    //     $key = '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';
-    //      $email =  new \SendGrid\Mail\Mail();
-    //      $email->setFrom("sundeepteki12@gmail.com","rtcamp");
-    //      $email->setSubject($subject);
-    //      $email->addTo($to);
-    //      $email->addContent("text/html",$content);
-    //      $sendgrid = new \SendGrid($key);
+        $key = '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';
+        //  $email =  new \SendGrid\Mail\Mail();
+        //  $email->setFrom("sundeepteki12@gmail.com","rtcamp");
+        //  $email->setSubject($subject);
+        //  $email->addTo($to);
+        //  $email->addContent("text/html",$content);
+        //  $sendgrid = new \SendGrid(getenv($key));
          
-    //     // Attachment code
-    //      $content = file_get_contents($attachment_content[0]);
-    //      $attachment = new \SendGrid\Mail\Attachment();
-    //      $attachment->setType("multipart/mixed");
-    //      $attachment->setFilename($attachment_content[1].".png");
-    //      $attachment->setContentId($attachment_content[1]);
-    //      $attachment->setDisposition("attachment");
-    //      $attachment->setContent($content);
-    //     $email->addAttachment($attachment);
-    //      try{
-    //          $response = $sendgrid->send($email);
-    //         echo "Mail sent successfully"; 
-    //      }catch(Exception $e){
-    //          echo 'Email exception Caught : '. $e->getMessage() ."\n";
-    //          return false;
-    //       }
+        // // Attachment code
+        //  $content = file_get_contents($attachment_content[0]);
+        //  $attachment = new \SendGrid\Mail\Attachment();
+        //  $attachment->setType("multipart/mixed");
+        //  $attachment->setFilename($attachment_content[1].".png");
+        //  $attachment->setContentId($attachment_content[1]);
+        //  $attachment->setDisposition("attachment");
+        //  $attachment->setContent($content);
+        // $email->addAttachment($attachment);
+        //  try{
+        //      $response = $sendgrid->send($email);
+        //     echo "Mail sent successfully"; 
+        //  }catch(Exception $e){
+        //      echo 'Email exception Caught : '. $e->getMessage() ."\n";
+        //      return false;
+        //   }
     
 $sendgrid = new SendGrid($key);
 $email    = new \SendGrid\Mail\Mail();
