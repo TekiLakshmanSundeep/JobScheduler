6d185895bca1f14d650feadbacdd528d6335a1bf
TekiLakshmanSundeep <tlakshmansundeep@yahoo.com>
2021-07-27 01:16:49 +0530

    Implemented clock file

--- console.php
+++ console.php
@@ -4,7 +4,7 @@ $cmd = "php $appRoot/console schedule:run";
 $outputPath = '/dev/null';
 $cmd = "$cmd > $outputPath &";
 $sleep = 60; //$input->getOption('sleep');
- 
+ echo $appRoot;
 while (true) {
     exec($cmd);
     echo 'Executed ok';
